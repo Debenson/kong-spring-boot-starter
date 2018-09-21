f5f0726b4b9809e489f2bab0138b3dd1ecec122d
Denys G. Santos <gsdenys@gmail.co>
2018-09-21 07:57:18 -0300

    first Beta Stable version

--- src/main/java/com/eimapi/starter/kong/bean/KongServiceImpl.java
+++ src/main/java/com/eimapi/starter/kong/bean/KongServiceImpl.java
@@ -1,5 +1,10 @@
 package com.eimapi.starter.kong.bean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -13,32 +18,59 @@ import com.eimapi.starter.kong.rest.ApiObject;
 @Component
 @Configuration
 public class KongServiceImpl implements KongService {
+	private Logger logger = LoggerFactory.getLogger(KongServiceImpl.class);
 
 	RestTemplate restTemplate = null;
 
+	@Value("${kong.service.url:http://localhost:8001}")
+	private String kongURL;
 
 	private RestTemplate getRestTemplate() {
 		if (restTemplate == null) {
 			this.restTemplate = new RestTemplate();
 		}
-		
+
 		return this.restTemplate;
 	}
 
 	@Override
 	public void addService(ApiObject api) {
-		
+
 		HttpEntity<ApiObject> apiEntity = new HttpEntity<>(api);
 		ResponseEntity<String> addAPIResp = null;
 
-		try {
-			addAPIResp = getRestTemplate().postForEntity("http://192.168.80.83:8001/apis", apiEntity, String.class);
-		} catch (HttpClientErrorException e) {
-			e.printStackTrace();
+		
+		ResponseEntity<String> respo = hasService(api.getName());
+		if (respo != null) {
+			try {
+				JSONObject jObj =  new JSONObject(respo.getBody());
+				String id = jObj.getString("id");
+				
+				getRestTemplate().delete(kongURL + "/apis/" + id);
+				
+				logger.info("The old APIs [" + api.getUris() + "] was removed from Kong.");
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
 		}
 
-		if (!addAPIResp.getStatusCode().equals(HttpStatus.CREATED)) {
-			System.err.println("\n\n\terro\n\n");
+		try {
+			addAPIResp = getRestTemplate().postForEntity((kongURL + "/apis"), apiEntity, String.class);
+		} catch (HttpClientErrorException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		if (addAPIResp.getStatusCode().equals(HttpStatus.CREATED)) {
+			logger.info("The APIs [" + api.getUris() + "] was created at Kong.");
+		}
+	}
+
+	public ResponseEntity<String> hasService(String name) {
+		try {
+			return getRestTemplate().getForEntity(this.kongURL + "/apis/" + name, String.class);
+		} catch (HttpClientErrorException e) {
+			return null;
 		}
 	}
 }
